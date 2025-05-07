e776586a5089a8e25a37c2c784bbbea7e8ec7932
Tg-Gupta1 <triyangupta122@gmail.com>
2025-05-07 11:25:26 +0530

    14commit

--- MorreVoting.cpp
+++ MorreVoting.cpp
@@ -32,3 +32,56 @@ class Solution {
             return -1;
         }
     };
+
+
+
+    // ----------------------------------------------------------------------
+    // n/3 majority element ..
+
+    class Solution {
+        public:
+            vector<int> majorityElement(vector<int>& nums) {
+                int cnt1=0;
+                int cnt2=0;
+                int ele1=NULL;
+                int ele2=NULL;
+                for(int i=0;i<nums.size();i++){
+                    if(ele1==nums[i]){
+                        cnt1++;
+                    }
+                    else if(ele2==nums[i]){
+                        cnt2++;
+                    }
+                    else if(cnt1==0){
+                        ele1=nums[i];
+                        cnt1=1;
+                    }
+                    else if(cnt2==0){
+                        ele2=nums[i];
+                        cnt2=1;
+                    }
+                    else{
+                        cnt1--;
+                        cnt2--;
+                    }
+                }
+                int freq1=0;
+                int freq2=0;
+                for(auto it:nums){
+                    if(it==ele1){
+                        freq1++;
+                    }
+                    else if(it==ele2){
+                        freq2++;
+                    }
+                }
+                vector<int> ans;
+                if(freq1 > floor(nums.size()/3)){
+                    ans.push_back(ele1);
+                }
+                if(freq2 > floor(nums.size()/3)){
+                    ans.push_back(ele2);
+                }
+                return ans;
+            }
+        };
